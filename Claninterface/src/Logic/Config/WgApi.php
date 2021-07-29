7248101d5997faad0727d8ce1ac26e10caff7f53
LFS96 <github@lfs96.de>
2021-07-29 11:59:42 +0200

    Anpassung an V1.4 von  artur-stepien / wargaming-papi TODO Config wieder nutzbar machen

--- Claninterface/src/Logic/Config/WgApi.php
+++ Claninterface/src/Logic/Config/WgApi.php
@@ -2,18 +2,18 @@
 namespace App\Logic\Config;
 
 use Cake\Core\Configure;
-use Cake\View\Helper\UrlHelper;
-use const Wargaming\SERVER_EU;
-use const Wargaming\LANGUAGE_DEUTSCH;
+use Wargaming\Language\EN as EnglishLanguage;
+use Wargaming\Server\EU as EuropeanServer;
 use Wargaming\API;
 
 class WgApi
 {
     public static function getWG_API (){
         $key = Configure::read('Wargaming.authkey');
-        $lang = Configure::read('Wargaming.lang');
-        $server = Configure::read('Wargaming.server');
-
-        return new API($key, $lang, $server);
+        $lang = new EnglishLanguage();
+        $server = new EuropeanServer($key);
+        $api = new API($lang, $server);
+        $api->setSSLVerification(false);
+        return $api;
     }
 }
